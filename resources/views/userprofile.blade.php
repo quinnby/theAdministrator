f0c8c5e1b519d6dd2fc1949f7d01c48e36e26a47
coombse <ellen.coombs@dcmail.ca>
2017-02-01 15:27:01 -0500

    Created user profile page

--- resources/views/userprofile.blade.php
+++ resources/views/userprofile.blade.php
@@ -7,31 +7,12 @@
 
 @section('main_container')
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <!-- Meta, title, CSS, favicons, etc. -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- page content -->
+    <div class="right_col" role="main">
 
-    <title>Gentelella Alela! | </title>
-
-    <!-- Bootstrap -->
-     <link href="{{ asset("css/bootstrap.min.css") }}" rel="stylesheet">
-    <!-- Font Awesome -->
-    <link href="{{ asset("css/font-awesome.min.css") }}" rel="stylesheet">
-    <!-- NProgress -->
-    <link href="{{ asset("css/nprogress.css") }}" rel="stylesheet">
-    <!-- bootstrap-daterangepicker -->
-    <link href="../vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
-    
-    <!-- Custom Theme Style -->
-    <link href="../build/css/custom.min.css" rel="stylesheet">
-  </head>
-
-  <body class="nav-md">
+    </div>
+    <!-- /page content -->
+    <body class="nav-md">
     <div class="container body">
       <div class="main_container">
         <div class="col-md-3 left_col">
@@ -42,12 +23,11 @@
 
             <div class="clearfix"></div>
 
-  
+            
 
             <br />
 
             
-
             <!-- /menu footer buttons -->
             <div class="sidebar-footer hidden-small">
               <a data-toggle="tooltip" data-placement="top" title="Settings">
@@ -67,7 +47,6 @@
           </div>
         </div>
 
-        
 
         <!-- page content -->
         <div class="right_col" role="main">
@@ -349,14 +328,7 @@
         </div>
         <!-- /page content -->
 
-        <!-- footer content -->
-        <footer>
-          <div class="pull-right">
-            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
-          </div>
-          <div class="clearfix"></div>
-        </footer>
-        <!-- /footer content -->
+        
       </div>
     </div>
 
@@ -486,5 +458,16 @@
     </script>
     <!-- /datepicker -->
   </body>
-</html>
+    
+            
+  
+              
+    <!-- footer content -->
+    <footer>
+        <div class="pull-right">
+            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+        </div>
+        <div class="clearfix"></div>
+    </footer>
+    <!-- /footer content -->
 @endsection
