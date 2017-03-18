6abaf1d0f83ef8a6619c083e668e70f6207f9024
William Beniuk <william.beniuk@dcmail.ca>
2017-03-17 21:45:15 -0400

    Got the week view working with booked off displaying days booked off as well as time working

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -45,18 +45,21 @@ class ScheduleController extends Controller
     
     public function viewWeek()
     {
+        //$monday = Carbon::now()->addDays(7)->startOfWeek();
         $monday = Carbon::now()->startOfWeek();
         $sunday = Carbon::now()->endOfWeek();
         $users = User::all();
         foreach ($users as $user)
         {
-            $schedule[$user->id]['schedule'] = $user->schedule->where('scheduleStart', '>=', '2017-03-17')->where('scheduleEnd', '<=', '2017-03-23')->sortByDesc('scheduleStart');
-            $schedule[$user->id]['bookoff'] = $user->bookedOff;
+            $schedule[$user->id]['schedule'] = $user->schedule->where('scheduleStart', '>=', '2017-03-17')->where('scheduleStart', '<=', '2017-03-20')->sortByDesc('scheduleStart');
+            $schedule[$user->id]['bookoff'] = $user->bookedOff->where('startDate', '<=', $sunday)->where('endDate', '>=', $monday);
         }
         //$schedule = User::find('3');
         //$schedule = $schedule->schedule;
         
         //$schedule = Schedule::all()->where('scheduleStart', '>=', '2017-03-17')->where('scheduleStart', '<=', '2017-03-23');
-        return $schedule;
+        //return $monday->format('d');
+        return view('schedule.index', compact('schedule', 'monday', 'users'));
+        return compact('schedule', 'monday', 'users');
     }
 }
---
+++ resources/views/schedule/index.blade.php
@@ -0,0 +1,93 @@
+@extends('layouts.blank') 
+@push('stylesheets')
+    <!-- Example -->
+    <!--<link href=" <link href="{{ asset("css/myFile.min.css") }}" rel="stylesheet">" rel="stylesheet">-->
+@endpush 
+@section('main_container')
+<!-- page content -->
+<div class="right_col" role="main">
+    <div class="">
+        <div class="page-title">
+            <div class="title_left">
+                <h3>Schedule</h3> <br/>
+                <a class="btn btn-round btn-default" href="{{ url('/schedule/create') }}"><i class="fa fa-arrow-circle-left m-right-xs"></i> Previous Week </a>
+                <a class="btn btn-round btn-default" href="{{ url('/schedule/create') }}"> Next Week <i class="fa fa-arrow-circle-right m-right-xs"></i></a>
+            </div>
+        </div>
+        
+        
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <br/>
+                <div class="x_panel">
+                    <div class="x_content">
+                        <h4>{{ $monday->format('F') }} {{ $monday->format('Y') }}</h4>
+                        <hr/>
+                        <div class="table-responsive">
+                            <table id="datatable" class="table table-striped jambo_table bulk_action">
+                                <thead>
+                                    <tr class="headings">
+                                        <th class="column-title">Staff </th>
+                                        <th class="column-title">Monday - {{ $monday->format('d') }} </th>
+                                        <th class="column-title">Tuesday - {{ $monday->format('d') + 1 }} </th>
+                                        <th class="column-title">Wednesday - {{ $monday->format('d') + 2 }} </th>
+                                        <th class="column-title">Thursday - {{ $monday->format('d') + 3 }} </th>
+                                        <th class="column-title">Friday - {{ $monday->format('d') + 4 }} </th>
+                                         <th class="column-title">Saturday - {{ $monday->format('d') + 5 }} </th>
+                                        <th class="column-title no-link last"><span class="nobr">Sunday - {{ $monday->format('d') + 6 }} </span></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($users as $user)
+                                    <tr><td>{{ $user->name }} {{$user->lastName}}</td>
+                                        <?php $scheduleIt = 0;
+                                            $bookedIt = 0; ?>
+                                        @for ($i = 0; $i < 7; $i++)
+                                            <td>
+                                                @if(count($schedule[$user->id]['schedule']) > 0 && count($schedule[$user->id]['schedule']) > $scheduleIt)
+                                                @for($inner = $scheduleIt; $inner < count($schedule[$user->id]['schedule']); $inner++)
+                                                    <?php $date = \Carbon\Carbon::parse($schedule[$user->id]['schedule'][$scheduleIt]['scheduleStart']) ?>
+                                                
+                                                @if($date->format('d') == ($monday->format('d') + $i))
+                                                    <?php $end = \Carbon\Carbon::parse($schedule[$user->id]['schedule'][$scheduleIt]['scheduleEnd']) ?>
+                                                <span>{{ $date->hour }}:{{ $date->minute }} -  {{ $end->hour }}:{{ $end->minute }}</span></br>
+                                                    <?php $scheduleIt++ ?>
+                                                @endif
+                                                @endfor
+                                                @endif
+                                        
+                                                @for($booked = 0; $booked < count($schedule[$user->id]['bookoff']); $booked++)
+                                                    <?php $bookStart = \Carbon\Carbon::parse($schedule[$user->id]['bookoff'][$booked]['startDate']);
+                                                        $bookEnd = \Carbon\Carbon::parse($schedule[$user->id]['bookoff'][$booked]['endDate']); ?>
+                                                    @if($monday->day + $i >= $bookStart->day && $monday->day + $i <= $bookEnd->day)
+                                                <span class="label label-warning">Booked Off</span></br>
+                                                    @endif
+                                                @endfor
+                                            </td>
+                                        @endfor 
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+<script>
+    $('#datatable').DataTable({
+        "aaSorting": [], //disables default sort
+        "columnDefs": 
+        [
+            {"orderable": false, "targets": [1,2,3,4,5,6,7]}
+        ]
+    });
+
+</script>
+<!-- /page content -->
+<!-- footer content -->
+<footer> @include('includes.footer') </footer>
+<!-- /footer content -->@endsection
