cce8eea609864924b7d4b0fd75a17e22e2804efe
William Beniuk <william.beniuk@dcmail.ca>
2017-03-21 16:43:33 -0400

    Can now seed database with users

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,9 +13,23 @@
 
 $factory->define(App\Models\User::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->safeEmail,
-        'password' => bcrypt(str_random(10)),
+        'name' => $faker->firstName,
+        'lastName' => $faker->lastName,
+        'email' => $faker->unique()->safeEmail,
+        'password' => bcrypt('password'),
         'remember_token' => str_random(10),
+        'birthDate' => $faker->date,
+        'hireDate' => $faker->date,
+        'address' => $faker->streetAddress,
+        'city' => 'Oshawa',
+        'postalCode' => 'L1L1L1',
+        'province' => 'ON',
+        'sinNumber' => '111-111-111',
+        'primaryPhone' => '(555)555-5555',
+        'secondaryPhone' => '(555)222-2222',
+        'titleId' => rand(1, 10),
+        'departmentId' => rand(1, 14),
+        'userTypeId' =>rand(2, 3),
+        
     ];
 });
